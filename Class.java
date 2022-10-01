e598d8b38f1e3512d1e14d5bd900feddf39d2e02
Chaehong Min <mch0618@gmail.com>
2022-09-30 22:30:20 -0400

    Initial sharing of project

---
+++ Class.java
@@ -0,0 +1,8 @@
+public class Class
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Hello Hello");
+        System.out.println("This was how i printed a line");
+    }
+}
---
+++ Melissa.java
@@ -0,0 +1,17 @@
+class Melissa
+
+{
+    public static void main (String[]args)
+    {
+        System.out.printf("Hello world\n");
+        System.out.printf("Hello world2\n");
+        
+        for (int i=0; i<10; i++)
+        {
+            for (int j=0; j<10; j++)
+            {
+                System.out.printf("%d x %d = %d\n", i, j, i*j);
+            }
+        }
+    }
+}  
